6cf6a4b86b9806bf2231259e3754fbae4789dd3a
SeanPhilippi <s.philippi@rsitex.com>
2020-04-27 13:02:56 -0500

    todo class added

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__) # __name__ references this file
+# tells apps where database is located
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # 3 /'s is relative path, 4 for absolute path
+# initialize database
+db = SQLAlchemy(app)
+
+# creating a model
+class Todo(db.Model):
+    id = db.Column(db.Intgeter, primary_key=True)
+    content = db.Column(db.String(200), nullable=False) # don't want this to be blank
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # representation
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/') # decorator
 
